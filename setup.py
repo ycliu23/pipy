f8a488eac6e4867a538cc9696f80391552ce6072
Yuchen Liu <85184435+ycliu23@users.noreply.github.com>
2023-05-16 17:00:16 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 VERSION = '0.0.13'
 DESCRIPTION = 'Images to Power spectrum Pipeline'
-LONG_DESCRIPTION = 'Python package that allows generation of 3D, 2D and 1D power spectra from images'
+
+with open("README.md"), encoding="utf-8") as f:
+    LONG_DESCRIPTION = f.read()
 
 setup(
     name = "pipy"
     version = VERSION
     authors = [{name="Yuchen Liu", email="yl871@cam.ac.uk"}]
     description = DESCRIPTION
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
+    packages=find_packages()
     readme = "README.md"
     url = "https://github.com/ycliu23/pipy"
+    keywords=["python","astrophysics","cosmology","cosmic dawn","reionization","power spectrum"],
     classifiers =
         {
             'License :: OSI Approved :: MIT License'
